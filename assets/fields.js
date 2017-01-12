04e00c3097c3046d14c589ac0e17298c08e54b7c
Filipe Menezes <filipepmenezes@gmail.com>
2017-01-11 22:30:03 -0200

    Campos adicionados dinâmicamente no formulário de tickets

--- assets/fields.js
+++ assets/fields.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
+$(window).load(function() {
   if(window.location.pathname.match(/ticket\.form\.php/)) {
-    console.log('INSERIR CAMPOS AQUI');
+    var i = setInterval(function() {
+      if($('#mainformtable').length) {
+        clearInterval(i);
+        inject_custom_fields();
+      }
+    }, 100);
+  }
+
+  function inject_custom_fields() {
+    $('#mainformtable').after("<div id='fortbrasil-container'></div>");
+    $('#fortbrasil-container').load('../plugins/fortbrasil/template/fields.html');
   }
 });
--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -3,11 +3,11 @@
 class PluginFortBrasilTicket {
 
   static function beforeCreate(Ticket $item) {
-    $item->input['name'] = $item->input['telefonefield'];
+    $item->input['name'] = $item->input['telefone_field'];
 
-    $id_conta = $item->input['idcontafield'];
-    $cpf      = $item->input['cpffield'];
-    $nome     = $item->input['nomefield'];
+    $id_conta = $item->input['id_conta_field'];
+    $cpf      = $item->input['cpf_field'];
+    $nome     = $item->input['nome_field'];
     $content  = $item->input['content'];
 
     $description = "ID Conta:\t$id_conta\nCPF:\t$cpf\nNome:\t$nome\n\n$content";
