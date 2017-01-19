a51cdbeda4b52f129c5f09bb360555e4cc8b5b7b
Filipe Menezes <filipepmenezes@gmail.com>
2017-01-18 22:59:29 -0200

    Valores dos campos carregados automaticamente

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -26,34 +26,45 @@ class PluginFortBrasilTicket extends CommonITILObject {
   }
 
   static function showCustomFields() {
+    $ticket_id  = self::getTicket();
+    $user       = self::getWatcher($ticket_id);
+
+    $id_conta   = ($user) ? $user->fields['name'] : null;
+    $nome       = ($user) ? $user->fields['firstname'] : null;
+    $cpf        = ($user) ? $user->fields['phone'] : null;
+    $produto    = ($user) ? $user->fields['realname'] : null;
+    $ddi        = ($user) ? substr($user->fields['mobile'], 0, 2) : null;
+    $telefone   = ($user) ? substr($user->fields['mobile'], 2, -1) : null;
+    $email      = '';
+
     echo "<table class='tab_cadre_fixe'>";
     echo "<tbody>";
 
     // ID Conta
     echo "<tr class='tab_bg_1'>";
     echo "<th width='13%'>ID Conta</th>";
-    echo "<td width='29%'><input type='text' name='id_conta_field' class='number'></td>";
+    echo "<td width='29%'><input type='text' name='id_conta_field' class='number' value='$id_conta'></td>";
     echo "<td colspan='2'></td>";
     echo "</tr>";
 
     // Nome
     echo "<tr class='tab_bg_1'>";
     echo "<th width='3%'>Nome</th>";
-    echo "<td width='29%'><input type='text' name='nome_field' value=''></td>";
+    echo "<td width='29%'><input type='text' name='nome_field' value='$nome'></td>";
     echo "<td colspan='2'></td>";
     echo "</tr>";
 
     // CPF
     echo "<tr class='tab_bg_1'>";
     echo "<th width='3%'>CPF</th>";
-    echo "<td width='29%'><input type='text' name='cpf_field' class='cpf'></td>";
+    echo "<td width='29%'><input type='text' name='cpf_field' class='cpf' value='$cpf'></td>";
     echo "<td colspan='2'></td>";
     echo "</tr>";
 
     // Produto
     echo "<tr class='tab_bg_1'>";
     echo "<th width='3%'>Produto</th>";
-    echo "<td width='29%'><input type='text' name='produto_field' value=''></td>";
+    echo "<td width='29%'><input type='text' name='produto_field' value='$produto'></td>";
     echo "<td colspan='2'></td>";
     echo "</tr>";
 
@@ -62,7 +73,7 @@ class PluginFortBrasilTicket extends CommonITILObject {
     echo "<th width='3%'>Telefone</th>";
     echo "<td width='29%'>" .
     "<input type='text' name='ddi_field' class='number' size='3' value='55'>" .
-    "<input type='text' name='telefone_field' class='telefone' size='14' style='margin-left: 2px'>" .
+    "<input type='text' name='telefone_field' class='telefone' value='$telefone' size='14' style='margin-left: 2px'>" .
     "</td>";
     echo "<td colspan='2'></td>";
     echo "</tr>";
@@ -87,13 +98,25 @@ class PluginFortBrasilTicket extends CommonITILObject {
 
     if(isset($parts['query'])) {
       parse_str($parts['query'], $query);
-      $id = $query['id'];
-
-      $ticket = new Ticket();
-      $ticket->getFromDB($id);
+      $ticket = $query['id'];
     }
 
     return $ticket;
+  }
+
+  private static function getWatcher($ticket_id) {
+    $user     = null;
+    $ticket   = new Ticket_User();
+
+    $user_id  = $ticket->find("tickets_id = $ticket_id AND type = 3", 'id', 1);
+    $user_id  = ($user_id) ? array_values($user_id)[0]['users_id'] : null;
+
+    if($user_id) {
+      $user = new User();
+      $user->getFromDB($user_id);
+    }
+
+    return $user;
   }
 }
 
