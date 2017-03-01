76a4b364275820e5a37b3820af941c6722a7ff55
Filipe Menezes <filipepmenezes@gmail.com>
2017-03-01 20:40:48 -0300

    Crontask refatorada

--- inc/cron.class.php
+++ inc/cron.class.php
@@ -1,9 +1,10 @@
 <?php
 
 class PluginFortbrasilCron {
+
   static function getTypeName($nb = 0) {
-      return 'FortBrasil';
-   }
+    return 'FortBrasil';
+  }
 
   static function cronInfo($name) {
     switch($name) {
@@ -15,23 +16,44 @@ class PluginFortbrasilCron {
   }
 
   static function cronImportUsers($task) {
+    $root     = GLPI_ROOT;
     $filename = $task->fields['comment'];
-    $data     = self::getFileData($filename);
+    $filename = "$root/plugins/fortbrasil/files/$filename";
+    $message  = '';
 
-    foreach($data as $row) {
-      $id_conta = $row['ID_CONTA'];
-      $cpf      = $row['CPF'];
-      $nome     = $row['NOME'];
-      $produto  = $row['PRODUTO'];
-      $ddd      = $row['DDD'];
-      $telefone = $row['TELEFONE'];
-      $email    = $row['EMAIL'];
+    if(is_file($filename)) {
+      $data = self::getFileData($filename);
 
-      $telefone = "55$ddd$telefone";
+      $created_users = 0;
+      $updated_users = 0;
 
-      self::insertUser($id_conta, $cpf, $nome, $produto, $telefone, $email);
+      foreach($data as $row) {
+        $user     = new User();
+        $user_id  = $user->getIdByName($row['ID_CONTA']);
+        $email    = $row['EMAIL'];
+
+        $user->fields['name']       = $row['ID_CONTA'];
+        $user->fields['phone']      = $row['CPF'];
+        $user->fields['firstname']  = $row['NOME'];
+        $user->fields['realname']   = $row['PRODUTO'];
+        $user->fields['mobile']     = '55' . $row['TELEFONE'];
+
+        if(!$user_id) {
+          self::insertUser($user, $email);
+          $created_users++;
+        } else {
+          $user->fields['id'] = $user_id;
+          self::updateUser($user, $email);
+          $updated_users++;
+        }
+      }
+
+      $message = "Inseridos: $created_users - Atualizados: $updated_users";
+    } else {
+      $message = "Não foi possível encontrar o arquivo $filename";
     }
 
+    $task->log($message);
     return 1;
   }
 
@@ -66,15 +88,7 @@ class PluginFortbrasilCron {
     return $results;
   }
 
-  static function insertUser($id_conta, $cpf, $nome, $produto, $telefone, $email) {
-    $user = new User();
-
-    $user->fields['name']       = $id_conta;
-    $user->fields['phone']      = $cpf;
-    $user->fields['firstname']  = $nome;
-    $user->fields['realname']   = $produto;
-    $user->fields['mobile']     = $telefone;
-
+  private static function insertUser($user, $email) {
     $user_id = $user->addToDB();
 
     if($user_id) {
@@ -84,7 +98,28 @@ class PluginFortbrasilCron {
     return $user_id;
   }
 
-  static function setDefaultEmail($user_id, $email) {
+  private static function updateUser($user, $email) {
+    global $DB;
+    $table = 'glpi_users';
+
+    $id_conta = $user->fields['name'];
+    $cpf      = $user->fields['phone'];
+    $nome     = $user->fields['firstname'];
+    $produto  = $user->fields['realname'];
+    $telefone = $user->fields['mobile'];
+
+    $query = "UPDATE `$table`
+              SET `phone` = '$cpf',
+              `firstname` = '$nome',
+              `realname` = '$produto',
+              `mobile` = '$telefone'
+              WHERE `name` = '$id_conta'";
+
+    $DB->query($query);
+    self::setDefaultEmail($user->fields['id'], $email);
+  }
+
+  private static function setDefaultEmail($user_id, $email) {
     $useremail    = new UserEmail();
     $email        = trim($email);
     $email_input  = array('email' => $email, 'users_id' => $user_id, 'is_default' => 0);
