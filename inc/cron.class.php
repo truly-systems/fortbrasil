d0ea5a6aa2fba96261d465a508cf9e19f6d34419
Filipe Menezes <filipepmenezes@gmail.com>
2017-03-15 10:03:31 -0300

    Crontask ajustada para executar o ETL

--- inc/cron.class.php
+++ inc/cron.class.php
@@ -16,115 +16,27 @@ class PluginFortbrasilCron {
   }
 
   static function cronImportUsers($task) {
-    $root     = GLPI_ROOT;
-    $filename = $task->fields['comment'];
-    $filename = "$root/plugins/fortbrasil/files/$filename";
-    $message  = '';
+    $root         = GLPI_ROOT;
+    $plugin_root  = "$root/plugins/fortbrasil";
+    $pdi_root     = "$plugin_root/lib/data-integration";
 
-    if(is_file($filename)) {
-      $data = self::getFileData($filename);
+    $conn     = DBConnection::getReadConnection();
+    $host     = $conn->dbhost;
+    $user     = $conn->dbuser;
+    $password = $conn->dbpassword;
+    $db       = $conn->dbdefault;
+    $port     = '3306';
 
-      $created_users = 0;
-      $updated_users = 0;
+    $file = $task->fields['comment'];
+    $file = "$plugin_root/files/$file";
 
-      foreach($data as $row) {
-        $user     = new User();
-        $user_id  = $user->getIdByName($row['ID_CONTA']);
-        $email    = $row['EMAIL'];
+    $cmd = "bash $pdi_root/pan.sh -file=$plugin_root/lib/import_users.ktr -param:HOST=$host -param:DB=$db -param:PORT=$port -param:USER=$user -param:PASSWORD=$password -param:FILE=$file";
+    exec($cmd, $out);
 
-        $user->fields['name']       = $row['ID_CONTA'];
-        $user->fields['phone']      = $row['CPF'];
-        $user->fields['firstname']  = $row['NOME'];
-        $user->fields['realname']   = $row['PRODUTO'];
-        $user->fields['mobile']     = '55' . $row['TELEFONE'];
-
-        if(!$user_id) {
-          self::insertUser($user, $email);
-          $created_users++;
-        } else {
-          $user->fields['id'] = $user_id;
-          self::updateUser($user, $email);
-          $updated_users++;
-        }
-      }
-
-      $message = "Inseridos: $created_users - Atualizados: $updated_users";
-    } else {
-      $message = "Não foi possível encontrar o arquivo $filename";
-    }
+    $message = 'Usuários importados com sucesso.';
 
     $task->log($message);
     return 1;
-  }
-
-  private static function getFileData($filename) {
-    $row = 0;
-    $col = 0;
-
-    $handle = @fopen($filename, 'r');
-
-    if($handle) {
-      while(($row = fgetcsv($handle, 4096, ';')) !== false) {
-        if(empty($fields)) {
-          $fields = $row;
-          continue;
-        }
-
-        foreach($row as $k=>$value) {
-          $results[$col][$fields[$k]] = trim($value);
-        }
-
-        $col++;
-        unset($row);
-      }
-
-      if(!feof($handle)) {
-        echo 'Error: unexpected fgets() failn';
-      }
-
-      fclose($handle);
-    }
-
-    return $results;
-  }
-
-  private static function insertUser($user, $email) {
-    $user_id = $user->addToDB();
-
-    if($user_id) {
-      self::setDefaultEmail($user_id, $email);
-    }
-
-    return $user_id;
-  }
-
-  private static function updateUser($user, $email) {
-    global $DB;
-    $table = 'glpi_users';
-
-    $id_conta = $user->fields['name'];
-    $cpf      = $user->fields['phone'];
-    $nome     = $user->fields['firstname'];
-    $produto  = $user->fields['realname'];
-    $telefone = $user->fields['mobile'];
-
-    $query = "UPDATE `$table`
-              SET `phone` = '$cpf',
-              `firstname` = '$nome',
-              `realname` = '$produto',
-              `mobile` = '$telefone'
-              WHERE `name` = '$id_conta'";
-
-    $DB->query($query);
-    self::setDefaultEmail($user->fields['id'], $email);
-  }
-
-  private static function setDefaultEmail($user_id, $email) {
-    $useremail    = new UserEmail();
-    $email        = trim($email);
-    $email_input  = array('email' => $email, 'users_id' => $user_id, 'is_default' => 0);
-
-    $useremail->add($email_input);
   }
 }
 
